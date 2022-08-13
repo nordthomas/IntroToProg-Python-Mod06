b1b2636a728fb0d46156f70737cb9baf66843eb8
nordthomas <110806051+nordthomas@users.noreply.github.com>
2022-08-13 00:18:29 -0700

    Add files via upload

---
+++ Assigment06_TNord.py
@@ -0,0 +1,189 @@
+# ---------------------------------------------------------------------------- #
+# Title: Assignment 06
+# Description: Working with functions in a class,
+#              When the program starts, load each "row" of data
+#              in "ToDoToDoList.txt" into a python Dictionary.
+#              Add the each dictionary "row" to a python list "table"
+# ChangeLog (Who,When,What):
+#   RRoot,1.1.2030,Created starter script
+#   TNord,8.11.2022,Got doc read/write code working as well as task entry
+#   TNord,8.12.2022,Got removing and saving data code working
+# ---------------------------------------------------------------------------- #
+
+# Data ---------------------------------------------------------------------- #
+# Declare variables and constants
+file_name_str = "ToDoFile.txt"  # The name of the data file
+file_obj = None  # An object that represents a file
+table_lst = []  # A list that acts as a 'table' of rows
+choice_str = ""  # Captures the user option selection
+
+
+# Processing  --------------------------------------------------------------- #
+class Processor():
+    """  Performs Processing tasks """
+
+    @staticmethod
+    def read_data_from_file(file_name, list_of_rows):
+        """ Reads data from a file into a list of dictionary rows
+
+        :param file_name: (string) with name of file:
+        :param list_of_rows: (list) you want filled with file data:
+        :return: (list) of dictionary rows
+        """
+        list_of_rows.clear()  # clear current data
+        file = open(file_name, "r")
+        for line in file:
+            task, priority = line.split(",")
+            row = {"Task": task.strip(), "Priority": priority.strip()}
+            list_of_rows.append(row)
+        file.close()
+        return list_of_rows
+
+    @staticmethod
+    def add_data_to_list(task_to_add, priority_to_add, list_of_rows):
+        """ Adds data to a list of dictionary rows
+
+        :param task_to_add: (string) with name of task:
+        :param priority_to_add: (string) with name of priority:
+        :param list_of_rows: (list) you want filled with file data:
+        :return: (list) of dictionary rows
+        """
+        row = {"Task": str(task_to_add).strip(), "Priority": str(priority_to_add).strip()}
+        list_of_rows.append(row)
+        print("\nTask added to list.")
+        return list_of_rows
+
+    @staticmethod
+    def remove_data_from_list(task_to_del, list_of_rows):
+        """ Removes data from a list of dictionary rows
+
+        :param task_to_del: (string) with name of task:
+        :param list_of_rows: (list) you want filled with file data:
+        :return: (list) of dictionary rows
+        """
+        for row in list_of_rows:
+            if row["Task"].lower() == task_to_del.lower():
+                list_of_rows.remove(row)
+                print("Task removed.")
+        return list_of_rows
+
+    @staticmethod
+    def write_data_to_file(file_name, list_of_rows):
+        """ Writes data from a list of dictionary rows to a File
+
+        :param file_name: (string) with name of file:
+        :param list_of_rows: (list) you want filled with file data:
+        :return: (list) of dictionary rows
+        """
+        print("******* The current items ToDo are: *******")
+        for row in list_of_rows:
+            print(row["Task"] + "(" + row["Priority"] + ")")
+        print("*******************************************")
+        objFile = open(file_name, "w")
+        for dicRow in list_of_rows:
+            objFile.write(dicRow["Task"] + "," + dicRow["Priority"] + "\n")
+        objFile.close()
+        input("Data saved to file! Press the [Enter] key to return to menu.")
+        return list_of_rows
+
+
+# Presentation (Input/Output)  -------------------------------------------- #
+
+
+class IO():
+    """ Performs Input and Output tasks """
+
+    @staticmethod
+    def output_menu_tasks():
+        """  Display a menu of choices to the user
+
+        :return: nothing
+        """
+        print('''
+        Menu of Options
+        1) Add a new Task
+        2) Remove an existing Task
+        3) Save Data to File        
+        4) Exit Program
+        ''')
+        print()  # Add an extra line for looks
+
+    @staticmethod
+    def input_menu_choice():
+        """ Gets the menu choice from a user
+
+        :return: string
+        """
+        choice = str(input("Which option would you like to perform? [1 to 4]: ")).strip()
+        print()  # Add an extra line for looks
+        return choice
+
+    @staticmethod
+    def output_current_tasks_in_list(list_of_rows):
+        """ Shows the current Tasks in the list of dictionaries rows
+
+        :param list_of_rows: (list) of rows you want to display
+        :return: nothing
+        """
+        print("******* The current tasks ToDo are: *******")
+        for row in list_of_rows:
+            print(row["Task"] + " (" + row["Priority"] + ")")
+        print("*******************************************")
+        print()  # Add an extra line for looks
+
+    @staticmethod
+    def input_new_task_and_priority():
+        """  Gets task and priority values to be added to the list
+
+        :return: (string, string) with task and priority
+        """
+        task = input("Enter a task: ")
+        priority = input("Enter a priority: ")
+        return task, priority
+
+    @staticmethod
+    def input_task_to_remove():
+        """  Gets the task name to be removed from the list
+
+        :return: (string) with task
+        """
+        remove = str(input("Enter the name of the task you wish to remove: ")).strip()
+        return remove
+
+
+# Main Body of Script  ------------------------------------------------------ #
+
+# Step 0 - When the program starts, if there is no file on disk, create one
+if file_obj == None:
+    file_obj = open(file_name_str, "a")
+    file_obj.close()
+
+# Step 1 - When the program starts, load data from ToDoFile.txt.
+Processor.read_data_from_file(file_name=file_name_str, list_of_rows=table_lst)  # read file data
+
+# Step 2 - Display a menu of choices to the user
+while (True):
+    # Step 3 Show current data
+    IO.output_current_tasks_in_list(list_of_rows=table_lst)  # Show current data in the list/table
+    IO.output_menu_tasks()  # Shows menu
+    choice_str = IO.input_menu_choice()  # Get menu option
+
+    # Step 4 - Process user's menu choice
+    if choice_str.strip() == '1':  # Add a new Task
+        task, priority = IO.input_new_task_and_priority()
+        table_lst = Processor.add_data_to_list(task_to_add=task, priority_to_add=priority, list_of_rows=table_lst)
+        continue  # to show the menu
+
+    elif choice_str == '2':  # Remove an existing Task
+        task = IO.input_task_to_remove()
+        table_lst = Processor.remove_data_from_list(task_to_del=task, list_of_rows=table_lst)
+        continue  # to show the menu
+
+    elif choice_str == '3':  # Save Data to File
+        table_lst = Processor .write_data_to_file(file_name=file_name_str, list_of_rows=table_lst)
+        print("Data Saved!")
+        continue  # to show the menu
+
+    elif choice_str == '4':  # Exit Program
+        print("Goodbye!")
+        break  # by exiting loop
